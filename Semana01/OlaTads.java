d7447594441dadc63411c7ebfa4c36ee5b50c07a
Vinicius <fritzenvinicius@gmail.com>
2025-02-27 20:37:08 -0300

    Semana 01 - primeiro programa

---
+++ Semana01/OlaTads.java
@@ -0,0 +1,17 @@
+public class OlaTads {
+    public static void main(String[] args) {
+        System.out.println("OI TADS!");
+        System.out.println("\tProfessor Vinicius");
+        System.out.println("1. LOGICA\n2. JAVA\n3. TADS");
+                                                    //   ^  obrigatorio
+        // PQ ESSE TEXTO FOI IGNORADO?
+        /* 
+        ESTE
+           TAMBÉM FOI IGNORADO?
+        */
+    }
+}
+
+// no terminal
+// javac OlaTads.java  => compila
+// java OlaTads        => executa
